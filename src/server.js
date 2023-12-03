253a720e949704893cddc3540872ff46c330677b
Sanchit Tiwari <78718126+SanchitTiwari@users.noreply.github.com>
2023-12-03 17:33:05 +0530

    temp3 Update server.js

--- src/server.js
+++ src/server.js
@@ -1,66 +1,50 @@
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const { createClient } = require('redis');
+const { ExpressPeerServer } = require("peer");
+const redisAdapter = require('@socket.io/redis-adapter');
 
 const app = express();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
-const { createClient } = require('redis');
-const redisAdapter = require('@socket.io/redis-adapter');
-
-const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(httpServer, {
   debug: true,
 });
 
+const publicDirectoryPath = path.join(__dirname, '../public');
 app.use("/peerjs", peerServer);
+app.use('/public', express.static(publicDirectoryPath));
 
- const pubClient = createClient({
-     host: process.env.REDIS_ENDPOINT || 'localhost',
-     port: process.env.REDIS_PORT || 6379
- });
+const pubClient = createClient({
+    host: process.env.REDIS_ENDPOINT || 'localhost',
+    port: process.env.REDIS_PORT || 6379
+});
 
+if (process.env.REDIS_PASSWORD) {
+    pubClient.auth(process.env.REDIS_PASSWORD);
+}
 
- if (process.env.REDIS_PASSWORD) {
-     pubClient.auth(process.env.REDIS_PASSWORD);
- }
-
-
- const subClient = pubClient.duplicate();
- io.adapter(redisAdapter(pubClient, subClient));
-
- 
- app.use('/public', express.static('../public'));
-
-
-const publicDirectoryPath = path.join(__dirname,'../public')
- 
-
-app.use('/public', express.static(publicDirectoryPath))
-
+const subClient = pubClient.duplicate();
+io.adapter(redisAdapter(pubClient, subClient));
 
 app.get('/:id', (req, res) => {
-    const fileDirectory = path.join(__dirname,'../');
+    const fileDirectory = path.join(__dirname, '../');
     res.sendFile('index.html', { root: fileDirectory }, (err) => {
         if (err) {
             console.error(err);
-            throw (err);
+            throw err;
         }
         res.end();
     });
 });
 
 app.get('/', (req, res) => {
-    console.log(',,',req)
-    let host = process.env.HOST_NAME || 'abc';
-    res.redirect(307, '/' + uuidv4() + '?host=' + host);
+    res.redirect(307, '/' + uuidv4());
 });
 
-
 io.on("connection", socket => {
-    console.log('socket connected..', socket.id);
-
-    console.log(process.env.HOST_NAME);
+    console.log('Socket connected:', socket.id);
 
     socket.on('content_change', (data) => {
         const room = data.documentId;
@@ -83,29 +67,28 @@ io.on("connection", socket => {
         socket.to(room).emit('register', { id: socket.id, name: data.handle });
     });
 
-     socket.on("join-room", (roomId, userId, userName) => {
+    socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
-        socket.to(roomId).broadcast.emit("user-connected", userId);
         socket.broadcast.to(roomId).emit("user-connected", userId);
-         socket.on("message", (message) => {
-         io.to(roomId).emit("createMessage", message, userName);
-         console.log(userName)
-         });
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+            console.log(userName);
+        });
     });
 
     socket.on("message", (data) => {
         console.log(data);
         io.to(data.id).emit("createMessage", data.message, data.name);
-        console.log(userName)
+        console.log(data.name);
     });
 
-    socket.on('disconnect', function (data) {
+    socket.on('disconnect', function () {
         console.log("Disconnected");
         socket.broadcast.emit('user_left', { id: socket.id });
     });
 });
 
-
-httpServer.listen(process.env.PORT || 3000  , () =>{
-    console.log('Server is up on port '+ 3000);
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+    console.log('Server is up on port ' + PORT);
 });
