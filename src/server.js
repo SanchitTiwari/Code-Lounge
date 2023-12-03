e161bee7f717860458e3d2919c3f09a0c31224aa
Sanchit Tiwari <78718126+SanchitTiwari@users.noreply.github.com>
2023-12-03 17:38:54 +0530

    temp 3 Update server.js

--- src/server.js
+++ src/server.js
@@ -1,50 +1,66 @@
 const express = require('express');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-const { createClient } = require('redis');
-const { ExpressPeerServer } = require("peer");
-const redisAdapter = require('@socket.io/redis-adapter');
 
 const app = express();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
+//const { createClient } = require('redis');
+//const redisAdapter = require('@socket.io/redis-adapter');
+
+const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(httpServer, {
   debug: true,
 });
 
-const publicDirectoryPath = path.join(__dirname, '../public');
 app.use("/peerjs", peerServer);
-app.use('/public', express.static(publicDirectoryPath));
 
-const pubClient = createClient({
-    host: process.env.REDIS_ENDPOINT || 'localhost',
-    port: process.env.REDIS_PORT || 6379
-});
+// const pubClient = createClient({
+//     host: process.env.REDIS_ENDPOINT || 'localhost',
+//     port: process.env.REDIS_PORT || 6379
+// });
 
-if (process.env.REDIS_PASSWORD) {
-    pubClient.auth(process.env.REDIS_PASSWORD);
-}
 
-const subClient = pubClient.duplicate();
-io.adapter(redisAdapter(pubClient, subClient));
+// if (process.env.REDIS_PASSWORD) {
+//     pubClient.auth(process.env.REDIS_PASSWORD);
+// }
+
+
+// const subClient = pubClient.duplicate();
+// io.adapter(redisAdapter(pubClient, subClient));
+
+// //beware
+// app.use('/public', express.static('../public'));
+
+// Define paths for Express Config
+const publicDirectoryPath = path.join(__dirname,'../public')
+ 
+//Setup static directory to serve
+app.use('/public', express.static(publicDirectoryPath))
+
 
 app.get('/:id', (req, res) => {
-    const fileDirectory = path.join(__dirname, '../');
+    const fileDirectory = path.join(__dirname,'../');
     res.sendFile('index.html', { root: fileDirectory }, (err) => {
         if (err) {
             console.error(err);
-            throw err;
+            throw (err);
         }
         res.end();
     });
 });
 
 app.get('/', (req, res) => {
-    res.redirect(307, '/' + uuidv4());
+    //console.log(',,',req)
+    let host = process.env.HOST_NAME || 'abc';
+    res.redirect(307, '/' + uuidv4() + '?host=' + host);
 });
 
+
 io.on("connection", socket => {
-    console.log('Socket connected:', socket.id);
+    console.log('socket connected..', socket.id);
+
+    console.log(process.env.HOST_NAME);
 
     socket.on('content_change', (data) => {
         const room = data.documentId;
@@ -67,28 +83,29 @@ io.on("connection", socket => {
         socket.to(room).emit('register', { id: socket.id, name: data.handle });
     });
 
-    socket.on("join-room", (roomId, userId, userName) => {
+     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
         socket.broadcast.to(roomId).emit("user-connected", userId);
         socket.on("message", (message) => {
-            io.to(roomId).emit("createMessage", message, userName);
-            console.log(userName);
+        io.to(roomId).emit("createMessage", message, userName);
+        console.log(userName)
         });
     });
 
     socket.on("message", (data) => {
         console.log(data);
         io.to(data.id).emit("createMessage", data.message, data.name);
-        console.log(data.name);
+        console.log(userName)
     });
 
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function (data) {
         console.log("Disconnected");
         socket.broadcast.emit('user_left', { id: socket.id });
     });
 });
 
-const PORT = process.env.PORT || 3000;
-httpServer.listen(PORT, () => {
-    console.log('Server is up on port ' + PORT);
+
+httpServer.listen(process.env.PORT || 3000  , () =>{
+    console.log('Server is up on port '+ 3000);
 });
