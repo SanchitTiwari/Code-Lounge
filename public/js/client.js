76148c668343503616e1ac34dc16b07eb118a445
Sanchit Tiwari <78718126+SanchitTiwari@users.noreply.github.com>
2023-12-03 19:50:37 +0530

    final Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,257 +1,244 @@
 window.onload = function () {
-  let socket;
-  const documentId = new URL(window.location.href).pathname.split('/')[1] || 'test';
-  let name = '';
-  let user = '';
-  let videoId = '';
+    let socket;
+    const documentId = new URL(window.location.href).pathname.split('/')[1] || 'test';
+    let name = '';
+    let user = '';
+    let videoId = '';
 
-  const handle = document.getElementById('handle');
-  const register = document.getElementById('register');
-  const registerPage = document.getElementById('registerPage');
+    const handle = document.getElementById('handle');
+    const register = document.getElementById('register');
+    const registerPage = document.getElementById('registerPage');
 
-  const editor = document.getElementById('editor');
-  const textarea = document.getElementById('textarea');
+    const editor = document.getElementById('editor');
+    const textarea = document.getElementById('textarea');
 
-  let syncValue = Array();
-  let keypressed = false;
+    let syncValue = Array();
+    let keypressed = false;
 
-  // CodeMirror configuration
-  var Codeeditor = CodeMirror.fromTextArea(textarea, {
-      styleActiveLine: true,
-      lineNumbers: true,
-      matchBrackets: true,
-      theme: 'cobalt',
-      mode: 'auto',
-  });
-  var minLines = 14;
-  var startingValue = '';
-  for (var i = 0; i < minLines; i++) {
-      startingValue += '\n';
-  }
-  Codeeditor.setValue(startingValue);
+    var Codeeditor = CodeMirror.fromTextArea(textarea, {
+        styleActiveLine: true,
+        lineNumbers: true,
+        matchBrackets: true,
+        theme: 'cobalt',
+        mode: 'auto',
+    });
 
-  let timeout = setTimeout(null, 0);
-  editor.addEventListener('keypress', () => {
-      clearTimeout(timeout);
-      keypressed = true;
-      timeout = setTimeout(applyLocalChanges, 1000);
-  });
+    function addEditor(writer) {
+        var ul = document.getElementById('editors');
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(writer.name));
+        li.className = 'list-group-item';
+        li.id = writer.id;
+        ul.appendChild(li);
+    }
 
-  function applyLocalChanges() {
-      if (keypressed) {
-          let currentData = Codeeditor.getValue();
-          let input = Array.from(syncValue);
-          let output = Array.from(currentData);
-          let changes = getChanges(input, output);
-          applyChanges(input, changes);
-          if (output.join('') == input.join('')) {
-              socket.emit('content_change', {
-                  documentId: documentId,
-                  changes: changes
-              });
-              syncValue = input;
-          }
-          keypressed = false;
-      }
-  }
+    function removeElement(id) {
+        var elem = document.getElementById(id);
+        if (elem) {
+            elem.parentNode.removeChild(elem);
+        }
+    }
 
-  function getChanges(input, output) {
-      return diffToChanges(diff(input, output), output);
-  }
+    function applyLocalChanges() {
+        if (keypressed) {
+            let currentData = Codeeditor.getValue();
+            let input = Array.from(syncValue);
+            let output = Array.from(currentData);
+            let changes = getChanges(input, output);
+            applyChanges(input, changes);
+            if (output.join('') == input.join('')) {
+                socket.emit('content_change', {
+                    documentId: documentId,
+                    changes: changes
+                });
+                syncValue = input;
+            }
+            keypressed = false;
+        }
+    }
 
-  function applyChanges(input, changes) {
-      changes.forEach(change => {
-          if (change.type == 'insert') {
-              input.splice(change.index, 0, ...change.values);
-          } else if (change.type == 'delete') {
-              input.splice(change.index, change.howMany);
-          }
-      });
-  }
+    function getChanges(input, output) {
+        return diffToChanges(diff(input, output), output);
+    }
 
-  function setSocketEvents() {
-      socket.on('content_change', (incomingChanges) => {
-          let input = Array.from(syncValue);
-          applyChanges(input, incomingChanges);
-          syncValue = input;
-          applyLocalChanges();
+    function applyChanges(input, changes) {
+        changes.forEach(change => {
+            if (change.type == 'insert') {
+                input.splice(change.index, 0, ...change.values);
+            } else if (change.type == 'delete') {
+                input.splice(change.index, change.howMany);
+            }
+        });
+    }
 
-          Codeeditor.setValue(syncValue.join(''));
-      });
+    function setSocketEvents() {
+        socket.on('content_change', (incomingChanges) => {
+            let input = Array.from(syncValue);
+            applyChanges(input, incomingChanges);
+            syncValue = input;
+            applyLocalChanges();
 
-      socket.on('register', (data) => {
-          addEditor(data);
-          videoId = data.id;
-      });
+            Codeeditor.setValue(syncValue.join(''));
+        });
 
-      socket.on('user_left', (data) => {
-          removeElement(data.id);
-      });
+        socket.on('register', (data) => {
+            addEditor(data);
+            videoId = data.id;
+        });
 
-      socket.on('members', (members) => {
-          members.forEach(member => {
-              addEditor(member);
-          });
-          socket.off('members');
-      });
-  }
+        socket.on('user_left', (data) => {
+            removeElement(data.id);
+        });
 
-  function addEditor(writer) {
-      var ul = document.getElementById('editors');
-      var li = document.createElement('li');
-      li.appendChild(document.createTextNode(writer.name));
-      li.className = 'list-group-item';
-      li.id = writer.id;
-      ul.appendChild(li);
-  }
+        socket.on('members', (members) => {
+            members.forEach(member => {
+                addEditor(member);
+            });
+            socket.off('members');
+        });
+    }
 
-  function removeElement(id) {
-      var elem = document.getElementById(id);
-      if (elem) {
-          elem.parentNode.removeChild(elem);
-      }
-  }
+    function registerUserListener() {
+        handle.style.display = 'none';
+        register.style.display = 'none';
+        registerPage.style.display = 'none';
 
-  function registerUserListener() {
-      handle.style.display = 'none';
-      register.style.display = 'none';
-      registerPage.style.display = 'none';
+        const editorBlock = document.getElementById('editor-block');
+        editorBlock.style.display = 'flex';
+        syncValue = '';
 
-      const editorBlock = document.getElementById('editor-block');
-      editorBlock.style.display = 'flex';
-      syncValue = '';
+        name = handle.value;
+        user = handle.value;
+        socket.emit('register', {
+            handle: handle.value,
+            documentId: documentId
+        });
+        setSocketEvents();
 
-      name = handle.value;
-      user = handle.value;
-      socket.emit('register', {
-          handle: handle.value,
-          documentId: documentId
-      });
-      setSocketEvents();
+        socket.on('createMessage', (message, userName) => {
+            messages.innerHTML =
+                messages.innerHTML +
+                `<div class="message">
+                    <b><i class="far fa-user-circle"></i> <span> ${
+                        userName === user ? 'me' : userName
+                    }</span> </b> 
+                    <span>${message}</span>
+                </div>`;
+        });
+    }
 
-      socket.on('createMessage', (message, userName) => {
-          messages.innerHTML =
-              messages.innerHTML +
-              `<div class="message">
-                  <b><i class="far fa-user-circle"></i> <span> ${
-                      userName === user ? 'me' : userName
-                  }</span> </b> 
-                  <span>${message}</span>
-              </div>`;
-      });
-  }
+    register.addEventListener('click', registerUserListener);
 
-  register.addEventListener('click', registerUserListener);
+    socket = io('/');
+    const ROOM_ID = documentId;
 
-  socket = io('/');
-  const ROOM_ID = documentId;
+    const videoGrid = document.getElementById('video-grid');
+    const myVideo = document.createElement('video');
+    myVideo.muted = true;
 
-  const videoGrid = document.getElementById('video-grid');
-  const myVideo = document.createElement('video');
-  myVideo.muted = true;
+    var peer = new Peer(undefined, {
+        path: '/peerjs',
+        host: '/',
+        port: '443',
+    });
 
-  var peer = new Peer(undefined, {
-      path: '/peerjs',
-      host: '/',
-      port: '443',
-  });
+    let myVideoStream;
 
-  let myVideoStream;
-  navigator.mediaDevices
-      .getUserMedia({
-          audio: true,
-          video: true,
-      })
-      .then((stream) => {
-          myVideoStream = stream;
-          addVideoStream(myVideo, stream);
+    navigator.mediaDevices
+        .getUserMedia({
+            audio: true,
+            video: true,
+        })
+        .then((stream) => {
+            myVideoStream = stream;
+            addVideoStream(myVideo, stream);
 
-          peer.on('call', (call) => {
-              call.answer(stream);
-              const video = document.createElement('video');
-              call.on('stream', (userVideoStream) => {
-                  addVideoStream(video, userVideoStream);
-              });
-          });
+            peer.on('call', (call) => {
+                call.answer(myVideoStream);
+                const video = document.createElement('video');
+                call.on('stream', (userVideoStream) => {
+                    addVideoStream(video, userVideoStream);
+                });
+            });
 
-          socket.on('user-connected', (userId) => {
-              connectToNewUser(userId, stream);
-          });
-      });
+            socket.on('user-connected', (userId) => {
+                connectToNewUser(userId, myVideoStream);
+            });
+        });
 
-  const connectToNewUser = (userId, stream) => {
-      const call = peer.call(userId, stream);
-      const video = document.createElement('video');
-      video.id = userId;
-      call.on('stream', (userVideoStream) => {
-          addVideoStream(video, userVideoStream);
-      });
-  };
+    const connectToNewUser = (userId, stream) => {
+        const call = peer.call(userId, stream);
+        const video = document.createElement('video');
+        video.id = userId;
+        call.on('stream', (userVideoStream) => {
+            addVideoStream(video, userVideoStream);
+        });
+    };
 
-  peer.on('open', (id) => {
-      socket.emit('join-room', ROOM_ID, id, user);
-  });
+    peer.on('open', (id) => {
+        socket.emit('join-room', ROOM_ID, id, user);
+    });
 
-  const addVideoStream = (video, stream) => {
-      video.srcObject = stream;
-      video.addEventListener('loadedmetadata', () => {
-          video.play();
-          videoGrid.append(video);
-      });
-  };
+    const addVideoStream = (video, stream) => {
+        video.srcObject = stream;
+        video.addEventListener('loadedmetadata', () => {
+            video.play();
+            videoGrid.append(video);
+        });
+    };
 
-  let text = document.querySelector('#chat_message');
-  let send = document.getElementById('send');
-  let messages = document.querySelector('.messages');
+    let text = document.querySelector('#chat_message');
+    let send = document.getElementById('send');
+    let messages = document.querySelector('.messages');
 
-  send.addEventListener('click', (e) => {
-      if (text.value.length !== 0) {
-          socket.emit('message', {
-              message: text.value,
-              id: documentId,
-              name: name
-          });
-          text.value = '';
-      }
-  });
+    send.addEventListener('click', (e) => {
+        if (text.value.length !== 0) {
+            socket.emit('message', {
+                message: text.value,
+                id: documentId,
+                name: name
+            });
+            text.value = '';
+        }
+    });
 
-  text.addEventListener('keydown', (e) => {
-      if (e.key === 'Enter' && text.value.length !== 0) {
-          socket.emit('message', {
-              message: text.value,
-              id: documentId,
-              name: name
-          });
-          text.value = '';
-      }
-  });
+    text.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' && text.value.length !== 0) {
+            socket.emit('message', {
+                message: text.value,
+                id: documentId,
+                name: name
+            });
+            text.value = '';
+        }
+    });
 
-  const inviteButton = document.querySelector('#inviteButton');
-  const muteButton = document.querySelector('#muteButton');
-  const stopVideo = document.querySelector('#stopVideo');
+    const inviteButton = document.querySelector('#inviteButton');
+    const muteButton = document.querySelector('#muteButton');
+    const stopVideo = document.querySelector('#stopVideo');
 
-  muteButton.addEventListener('click', () => {
-      const enabled = myVideoStream.getAudioTracks()[0].enabled;
-      myVideoStream.getAudioTracks()[0].enabled = !enabled;
-      updateButtonState(muteButton, 'fas fa-microphone', 'fas fa-microphone-slash', enabled);
-  });
+    muteButton.addEventListener('click', () => {
+        const enabled = myVideoStream.getAudioTracks()[0].enabled;
+        myVideoStream.getAudioTracks()[0].enabled = !enabled;
+        updateButtonState(muteButton, 'fas fa-microphone', 'fas fa-microphone-slash', enabled);
+    });
 
-  stopVideo.addEventListener('click', () => {
-      const enabled = myVideoStream.getVideoTracks()[0].enabled;
-      myVideoStream.getVideoTracks()[0].enabled = !enabled;
-      updateButtonState(stopVideo, 'fas fa-video', 'fas fa-video-slash', enabled);
-  });
+    stopVideo.addEventListener('click', () => {
+        const enabled = myVideoStream.getVideoTracks()[0].enabled;
+        myVideoStream.getVideoTracks()[0].enabled = !enabled;
+        updateButtonState(stopVideo, 'fas fa-video', 'fas fa-video-slash', enabled);
+    });
 
-  inviteButton.addEventListener('click', (e) => {
-      prompt(
-          'Copy this link and send it to people you want to meet with',
-          window.location.href
-      );
-  });
+    inviteButton.addEventListener('click', (e) => {
+        prompt(
+            'Copy this link and send it to people you want to meet with',
+            window.location.href
+        );
+    });
 
-  function updateButtonState(button, iconEnabled, iconDisabled, enabled) {
-      const html = `<i class="${enabled ? iconEnabled : iconDisabled}"></i>`;
-      button.classList.toggle('background__red');
-      button.innerHTML = html;
-  }
+    function updateButtonState(button, iconEnabled, iconDisabled, enabled) {
+        const html = `<i class="${enabled ? iconEnabled : iconDisabled}"></i>`;
+        button.classList.toggle('background__red');
+        button.innerHTML = html;
+    }
 };
