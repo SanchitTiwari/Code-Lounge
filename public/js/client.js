9dffb14bdd9d87ce6d54039cca50ecebe27b37fc
Sanchit Tiwari <78718126+SanchitTiwari@users.noreply.github.com>
2023-12-16 10:00:16 +0530

    final Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -19,7 +19,7 @@ window.onload = function () {
               theme: 'dracula',
               mode: "text/x-csrc",
           }); 
-    var minLines = 14;
+    var minLines = 2;
     var startingValue = '';
     for (var i = 0; i < minLines; i++) {
       startingValue += '\n';
